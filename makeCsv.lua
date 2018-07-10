e986f48c1f15d438660d1f28588b51b5921e1417
René Hernández <rene.rhc@gmail.com>
2018-07-10 14:22:59 -0600

    add command line argument for file with words

--- makeCsv.lua
+++ makeCsv.lua
@@ -1,32 +1,39 @@
 local lookDatabase = require("lookDatabase")
 
-plants = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap", "Fireweed", "Frostweed",
+keyProfiles = lookDatabase.getKeyProfiles()
+
+words = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap", "Fireweed", "Frostweed",
 "Furry Caged Beast", "Caged Mighty Wolf", "Sumptuous Fur",
 "True Iron Ore", "Blackrock Ore",
 "Alchemical Catalyst", "Sorcerous Earth", "Sorcerous Air", "Sorcerous Fire", "Sorcerous Water"}
 
-file = io.open("allItems.csv", "w")
+filename_csv = "allItems.csv"
+
+if arg[1] then
+    words = lookDatabase.lines_from_file(arg[1])
+    filename_csv = string.match(arg[1], "^[%a%s-]+")..".csv"
+end
+
+file = io.open(filename_csv, "w")
 io.output(file)
 io.write("character, ")
-for index, keyWord in pairs(plants) do
-    if index < #plants then
+for index, keyWord in pairs(words) do
+    if index < #words then
         io.write(keyWord, ", ")
     else
         io.write(keyWord, ",\n")
     end
 end
 
-keyProfiles = lookDatabase.getKeyProfiles()
-
 for index, keyProfile in pairs(keyProfiles) do
     io.write(keyProfile:match("^[%a]+")..",")
-    for index, keyWord in pairs(plants) do
+    for index, keyWord in pairs(words) do
         listFound = lookDatabase.searchWord(keyProfile, keyWord)
         total = 0
         for keyFound, found in pairs(listFound) do
             total = total + found.count
         end
-        if index < #plants then
+        if index < #words then
             io.write(string.format("%d,", total))
         else
             io.write(string.format("%d", total))
