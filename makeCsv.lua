e9bd78a6f48b534f13fa307103b382510ffe3afa
René Hernández <rene.rhc@gmail.com>
2018-07-10 13:37:06 -0600

    make used of mudule

--- makeCsv.lua
+++ makeCsv.lua
@@ -1,38 +1,9 @@
-require("ArkInventory")
+local lookDatabase = require("lookDatabase")
 
-function searchWord(keyProfile, keyWord)
-    listFound = {}
-    for keyLocation, location in pairs(ARKINVDB.global.player.data[keyProfile].location) do
-        for keyBag, bag in pairs(location.bag) do
-            if bag.slot then
-                for keySlot, slot in pairs(bag.slot) do
-                    if slot.h then
-                        encontrado = slot.h:find(keyWord)
-                        if encontrado then
-                            itemsFound = {h = slot.h, count = slot.count}
-                            listFound[#listFound + 1] = itemsFound
-                        end
-                    end
-                end
-            end
-        end
-    end
-    return listFound
-end
-
-function displayFounds(listFound)
-    for keyFound, itemFound in pairs(listFound) do
-        print(itemFound.h..",  "..itemFound.count)
-    end
-end
-
-furryCages = {"Furry Caged Beast", "Caged Mighty Wolf"}
 plants = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap", "Fireweed", "Frostweed",
 "Furry Caged Beast", "Caged Mighty Wolf", "Sumptuous Fur",
 "True Iron Ore", "Blackrock Ore",
 "Alchemical Catalyst", "Sorcerous Earth", "Sorcerous Air", "Sorcerous Fire", "Sorcerous Water"}
-ore = {"True Iron Ore", "Blackrock Ore"}
-mats = {"Sumptuous Fur"}
 
 file = io.open("allItems.csv", "w")
 io.output(file)
@@ -45,10 +16,12 @@ for index, keyWord in pairs(plants) do
     end
 end
 
-for keyProfile, profile in pairs(ARKINVDB.profileKeys) do
+keyProfiles = lookDatabase.getKeyProfiles()
+
+for index, keyProfile in pairs(keyProfiles) do
     io.write(keyProfile:match("^[%a]+")..",")
     for index, keyWord in pairs(plants) do
-        listFound = searchWord(keyProfile, keyWord)
+        listFound = lookDatabase.searchWord(keyProfile, keyWord)
         total = 0
         for keyFound, found in pairs(listFound) do
             total = total + found.count
