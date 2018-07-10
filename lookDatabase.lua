ef3d429546a1acaffcb0001bf4981d522cc3a1ee
René Hernández <rene.rhc@gmail.com>
2018-07-10 13:32:54 -0600

    create module for easy programming

---
+++ lookDatabase.lua
@@ -0,0 +1,42 @@
+require("ArkInventory")
+
+local lookDatabase = {}
+-- ARKINVDB.global.player.data["Zhok - Magtheridon"].location[1].bag[1].slot[1].h
+-- for k in pairs(ARKINVDB.global.player.data["Zhok - Magtheridon"].location[1].bag) do print(k) end
+
+-- file = io.open("jaulas.csv", "w")
+function lookDatabase.searchWord(keyProfile, keyWord)
+    listFound = {}
+    for keyLocation, location in pairs(ARKINVDB.global.player.data[keyProfile].location) do
+        for keyBag, bag in pairs(location.bag) do
+            if bag.slot then
+                for keySlot, slot in pairs(bag.slot) do
+                    if slot.h then
+                        encontrado = slot.h:find(keyWord)
+                        if encontrado then
+                            itemsFound = {h = slot.h, count = slot.count}
+                            listFound[#listFound + 1] = itemsFound
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return listFound
+end
+
+function lookDatabase.displayListFound(listFound)
+    for keyFound, itemFound in pairs(listFound) do
+        io.write(itemFound.h, ",  ", itemFound.count)
+    end
+end
+
+function lookDatabase.getKeyProfiles()
+    keyProfiles = {}
+    for keyProfile, profile in pairs(ARKINVDB.profileKeys) do
+        keyProfiles[#keyProfiles+1] = keyProfile
+    end
+    return keyProfiles
+end
+
+return lookDatabase
