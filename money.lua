46eb3cff37e69d7803fe6cb32a5a65efcdd9117e
René Hernández <rene.rhc@gmail.com>
2018-07-09 21:58:45 -0600

    pause console

--- money.lua
+++ money.lua
@@ -3,5 +3,5 @@ total = 0
 for key, value in pairs(ARKINVDB.profileKeys) do
     total = total + ARKINVDB.global.player.data[key].info.money
 end
-
-print("Total: ", total)
+print("Total Gold: ", total)
+os.execute("pause")
