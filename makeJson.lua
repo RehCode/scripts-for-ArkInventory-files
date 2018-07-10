498e3f2dbeb7413a9d39f41d3dbb596c1a7a11c5
René Hernández <rene.rhc@gmail.com>
2018-07-10 14:35:54 -0600

    use of module lookDatabase and line commands

--- makeJson.lua
+++ makeJson.lua
@@ -1,45 +1,27 @@
-require("ArkInventory")
 local json = require("dkjson")
+local lookDatabase = require("lookDatabase")
 
-function searchWord(keyProfile, keyWord)
-    listFound = {}
-    for keyLocation, location in pairs(ARKINVDB.global.player.data[keyProfile].location) do
-        for keyBag, bag in pairs(location.bag) do
-            if bag.slot then
-                for keySlot, slot in pairs(bag.slot) do
-                    if slot.h then
-                        encontrado = slot.h:find(keyWord)
-                        if encontrado then
-                            itemsFound = {h = slot.h, count = slot.count}
-                            listFound[#listFound + 1] = itemsFound
-                        end
-                    end
-                end
-            end
-        end
-    end
-    return listFound
-end
+keyProfiles = lookDatabase.getKeyProfiles()
 
-function displayFounds(listFound)
-    for keyFound, itemFound in pairs(listFound) do
-        print(itemFound.h..",  "..itemFound.count)
-    end
-end
-
-plants = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap", "Fireweed", "Frostweed",
+words = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap", "Fireweed", "Frostweed",
 "Furry Caged Beast", "Caged Mighty Wolf", "Sumptuous Fur",
 "True Iron Ore", "Blackrock Ore",
 "Alchemical Catalyst", "Sorcerous Earth", "Sorcerous Air", "Sorcerous Fire", "Sorcerous Water"}
 
+filename = "allItems.json"
+
+if arg[1] then
+    words = lookDatabase.lines_from_file(arg[1])
+    filename = string.match(arg[1], "^[%a%s-]+")..".json"
+end
 
 local dataJson = {}
-for keyProfile, profile in pairs(ARKINVDB.profileKeys) do
+for index, keyProfile in pairs(keyProfiles) do
     local profile_name = keyProfile:match("^[%a]+")
     dataJson[#dataJson+1] = {name = profile_name, items = {}}
 
-    for index, keyWord in pairs(plants) do
-        listFound = searchWord(keyProfile, keyWord)
+    for index, keyWord in pairs(words) do
+        listFound = lookDatabase.searchWord(keyProfile, keyWord)
         total = 0
         for keyFound, found in pairs(listFound) do
             total = total + found.count
@@ -48,7 +30,7 @@ for keyProfile, profile in pairs(ARKINVDB.profileKeys) do
     end
 end
 
-local file_json = io.open("test.json", "w")
+local file_json = io.open(filename, "w")
 local raw = json.encode(dataJson)
 file_json:write(raw)
 file_json:close()
