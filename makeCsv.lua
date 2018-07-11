799ccb63532f68295c9139c207bf982d203f71f8
René Hernández <rene.rhc@gmail.com>
2018-07-10 20:58:01 -0600

    refractor, now can save json and csv

--- makeCsv.lua
+++ makeCsv.lua
@@ -1,3 +1,4 @@
+local json = require("dkjson")
 local lookDatabase = require("lookDatabase")
 
 keyProfiles = lookDatabase.getKeyProfiles()
@@ -8,40 +9,55 @@ words = {"Talador Orchid", "Nagrand Arrowbloom", "Starflower", "Gorgrond Flytrap
 "Alchemical Catalyst", "Sorcerous Earth", "Sorcerous Air", "Sorcerous Fire", "Sorcerous Water"}
 
 data_dir = "data/"
-ext = ".csv"
-filename = "allItems-" .. os.time() .. ext
+filename = "allItems-" .. os.time()
 
 if arg[1] then
     words = lookDatabase.lines_from_file(arg[1])
-    filename = string.match(arg[1], "^[%a%s-]+").. os.time() .. ext
+    filename = string.match(arg[1], "^[%a%s-]+") .. os.time()
 end
 
-file = io.open(data_dir..filename, "w")
-io.output(file)
-io.write("character, ")
-for index, keyWord in pairs(words) do
-    if index < #words then
-        io.write(keyWord, ", ")
-    else
-        io.write(keyWord, ",\n")
-    end
-end
-
+local dataFound = {}
 for index, keyProfile in pairs(keyProfiles) do
-    io.write(keyProfile:match("^[%a]+")..",")
+    local profile_name = keyProfile:match("^[%a]+")
+    dataFound[#dataFound+1] = {name = profile_name, items = {}}
     for index, keyWord in pairs(words) do
         listFound = lookDatabase.searchWord(keyProfile, keyWord)
         total = 0
         for keyFound, found in pairs(listFound) do
             total = total + found.count
         end
-        if index < #words then
-            io.write(string.format("%d,", total))
-        else
-            io.write(string.format("%d", total))
-        end
+        dataFound[#dataFound].items[keyWord] = total
     end
-    io.write(",\n")
 end
 
-io.close(file)
+function save_json()
+    filename = filename .. ".json"
+    local file_json = io.open(data_dir..filename, "w")
+    local raw = json.encode(dataFound)
+    file_json:write(raw)
+    file_json:close()
+end
+
+function save_csv()
+    filename = filename .. ".csv"
+    file_csv = io.open(data_dir..filename, "w")
+
+    file_csv:write("character, ")
+    for index, keyWord in pairs(words) do
+        file_csv:write(keyWord, ", ")
+    end
+    file_csv:write("\n")
+
+    for index, data in pairs(dataFound) do
+        file_csv:write(data['name'], ", ")
+        for keyItemName, itemCount in pairs(data.items) do
+            file_csv:write(itemCount, ", ")
+        end
+        file_csv:write(",\n")
+    end
+
+    file_csv:close(file)
+end
+
+save_csv()
+save_json()
