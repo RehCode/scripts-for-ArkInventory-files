e1c407271c95807c3e39caf69dcd2c4e1f6ccba2
René Hernández <rene.rhc@gmail.com>
2018-07-11 20:44:46 -0600

    show how the count for the search term

--- search.lua
+++ search.lua
@@ -4,24 +4,54 @@ keyProfiles = lookDatabase.getKeyProfiles()
 
 if arg[1] then
     keyWord = arg[1]
-    print(keyWord)
-end
-listFound = nil
-local dataFound = {}
-for index, keyProfile in pairs(keyProfiles) do
-    -- local profile_name = keyProfile:match("^[%a]+")
-    -- dataFound[#dataFound+1] = {name = profile_name, items = {}}
-    listFound = lookDatabase.searchWord(keyProfile, keyWord)
-    io.write(keyProfile, "\n")
-    for keyFound, itemFound in pairs(listFound) do
-        if itemFound then
-            io.write("\t", itemFound.h, "==>  ", itemFound.count,"\n")
-        end
-    end
-    -- total = 0
-    -- for keyFound, found in pairs(listFound) do
-    --     total = total + found.count
-    -- end
-    -- dataFound[#dataFound].items[keyWord] = total
+    io.write("Search: ", keyWord, "\n")
+else
+    io.write("Need word")
+    return
 end
 
+local wordsCountAll = {}
+for index, keyProfile in pairs(keyProfiles) do
+    local wordsSet = {}
+    local wordsCount = {}
+    local showProfileName = false
+    listFound = lookDatabase.searchWord(keyProfile, keyWord)
+    for index, slot in pairs(listFound) do
+        if slot then
+            showProfileName = true
+            itemName = slot.h:match("|h%[(.+)%]")
+            if itemName then
+                if not wordsSet[itemName] then
+                    wordsSet[itemName] = true
+                    wordsCount[itemName] = {count = slot.count, h = slot.h}
+                else
+                    wordsCount[itemName].count = wordsCount[itemName].count + slot.count
+                end
+            end
+        end
+    end
+
+    if showProfileName then
+        name = keyProfile:match("%a+")
+        io.write("\n", name, "\n")
+        showProfileName = false
+    end
+
+    for itemName, item in pairs(wordsCount) do
+        io.write(string.format("\t%s: %d\n", itemName, item.count))
+
+        if not wordsCountAll[itemName] then
+            wordsCountAll[itemName] = {count = item.count, h = item.h}
+        else
+            wordsCountAll[itemName].count = wordsCount[itemName].count + item.count
+        end
+
+    end
+    
+end
+io.write("\n")
+io.write(string.rep("-", 27))
+io.write(" Total: \n")
+for itemName, item in pairs(wordsCountAll) do
+    io.write(string.format("\t%s: %d\n", itemName, item.count))
+end
