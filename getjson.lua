15e6d1e8203733a0095d85dfd8b9b7794f00a3c3
René Hernández <rene.rhc@gmail.com>
2018-07-14 13:32:51 -0600

    fix null json on arg save

--- getjson.lua
+++ getjson.lua
@@ -14,7 +14,6 @@ elseif arg[1] == "save" then
     local data_dir = "data/"
     local filename = "arkinventory-" .. os.time() .. ".json"
     local file_json = io.open(data_dir..filename, "w")
-    local raw = json.encode(dataFound)
     file_json:write(raw)
     file_json:close()
     io.write("Save database in file: ", filename)
