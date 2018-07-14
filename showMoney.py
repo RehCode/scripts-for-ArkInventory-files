033920d8355eb051bed9696cbea599fa487c0d65
René Hernández <rene.rhc@gmail.com>
2018-07-14 13:32:22 -0600

    show diferente total of money

--- showMoney.py
+++ showMoney.py
@@ -1,31 +1,41 @@
 import json
-import re
 import subprocess
-
-# with open("database.json", "r") as file:
-#     database = json.load(file)
+import glob
 
 data_json = subprocess.check_output(["lua", "getjson.lua", "get"])
 database = json.loads(data_json.decode())
 
-re_name = re.compile("^\w+")
-profileKeys = []
-for keyProfile in database['profileKeys'].keys():
-    profileKeys.append(keyProfile)
-    # name = re_name.match(keyProfile)
-    # if name:
-    #     print(name.group())
+filesname_database = glob.glob("data/arkinventory-*.json")
+# open old database
+with open(filesname_database[-1], "r") as oldfile:
+    database_old = json.load(oldfile)
 
-total = 0
-# show moneys
-# database['global']['player']['data']['Zhok - Magtheridon']['info']['money']
-for keyProfile in profileKeys:
-    name = database['global']['player']['data'][keyProfile]['info']['name']
+def get_profile_keys(database):
+    profileKeys = []
+    for keyProfile in database['profileKeys'].keys():
+        profileKeys.append(keyProfile)
+    return profileKeys
 
-    if 'money' in database['global']['player']['data'][keyProfile]['info']:
-        money = database['global']['player']['data'][keyProfile]['info']['money']
-        print("{:<8} {:>18,}".format(name + ":", money / 1e4))
-        total += money
+def getMoneys(database):
+    moneys = {"total":0, "profiles":[]}
+    # show moneys
+    # database['global']['player']['data']['Zhok - Magtheridon']['info']['money']
+    for keyProfile in database['profileKeys'].keys():
+        name = database['global']['player']['data'][keyProfile]['info']['name']
+        if 'money' in database['global']['player']['data'][keyProfile]['info']:
+            money = database['global']['player']['data'][keyProfile]['info']['money']
+            moneys["total"] += money
+        moneys["profiles"].append({"name":name, "money":money})
+    return moneys
 
+moneys = getMoneys(database)
+
+for profile in moneys["profiles"]:
+    print("{:<8} {:>18,}".format(profile["name"] + ":", profile["money"] / 1e4))
 print("-"*27)
-print("Total: {:>20,}".format(total/1e4))
+print("Total: {:>20,}".format(moneys["total"] / 1e4))
+
+moneys2 = getMoneys(database_old)
+print("-"*27)
+print("Old Total: {:>16,}".format(moneys2["total"] / 1e4))
+print("Difference: {:>15,}".format((moneys["total"] - moneys2["total"]) / 1e4))
